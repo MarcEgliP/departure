301e015575735c9fdad580a59548ce218943a335
Yanick Minder <minder@puzzle.ch>
2023-05-02 14:17:00 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {Dashboard} from "./Dashboard";
+import {Login} from "./Login";
 
 function App() {
-  return (
-   <div className="display-2">
-     Test
-   </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Dashboard</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Dashboard/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
