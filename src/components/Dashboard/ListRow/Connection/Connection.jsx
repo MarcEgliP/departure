dd4f71aa0a51abdb15373a69085074c8f988e21d
megli2 <egli@puzzle.ch>
2023-05-08 13:10:35 +0200

    restyling of connections-component

--- src/components/Dashboard/ListRow/Connection/Connection.jsx
+++ src/components/Dashboard/ListRow/Connection/Connection.jsx
@@ -8,7 +8,16 @@ export function Connection({connection, index}) {
                 <h2 className="accordion-header" id="headingOne">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target={"#departure" + index}>
-                        <p className="fs-2"><b>Abfahrt: </b>{moment(connection.from.departure).fromNow()}</p>
+                        <div className="d-flex flex-row">
+                            <div className="fs-3 w-100 d-flex flex-row">
+                                <b className="d-flex">Verbindung: </b>
+                                <i className="bi bi-clock ms-3">
+                                    <i className="ms-3">
+                                        {moment(connection.from.departure).format("hh:mm:ss")}
+                                    </i>
+                                </i>
+                            </div>
+                        </div>
                     </button>
                 </h2>
                 <div id={"departure" + index} className="accordion-collapse collapse"
@@ -16,7 +25,10 @@ export function Connection({connection, index}) {
                     <div className="accordion-body d-flex flex-row fs-5">
                         <div className="w-50">
                             <p><b>Startstation: </b>{connection.from.station.name}</p>
-                            <p><b>Verspätung: </b>{connection.from.delay + " Minuten"}</p>
+                            <p><b>Verspätung  : </b>{connection.from.delay + " Minuten"}</p>
+                            <p>
+                                <b>Abfahrt in: </b>{moment(connection.from.departure).fromNow("hh:mm:ss")}
+                            </p>
                         </div>
                         <div className="w-50">
                             <p><b>Endstation: </b>{connection.to.station.name}</p>
