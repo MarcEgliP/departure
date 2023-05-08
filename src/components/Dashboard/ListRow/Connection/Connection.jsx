6c63aae08519c14e150bdfec42c7b7c40282ee49
megli2 <egli@puzzle.ch>
2023-05-08 07:29:38 +0200

    add data to connection component and integrate moment.js

--- src/components/Dashboard/ListRow/Connection/Connection.jsx
+++ src/components/Dashboard/ListRow/Connection/Connection.jsx
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export function Connection({connection, index}) {
 
     return (
@@ -6,13 +8,20 @@ export function Connection({connection, index}) {
                 <h2 className="accordion-header" id="headingOne">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target={"#departure" + index}>
-                        <p>Departure: {connection.from.departure}</p>
+                        <p><b>Abfahrt: </b>{moment(connection.from.departure).fromNow()}</p>
                     </button>
                 </h2>
                 <div id={"departure" + index} className="accordion-collapse collapse"
                      data-bs-parent="#accordionDepartures">
-                    <div className="accordion-body">
-                        <h1>Moin</h1>
+                    <div className="accordion-body d-flex flex-row">
+                        <div className="w-50">
+                            <p><b>Startstation: </b>{connection.from.station.name}</p>
+                            <p><b>Verspätung: </b>{connection.from.delay + " Minuten"}</p>
+                        </div>
+                        <div className="w-50">
+                            <p><b>Endstation: </b>{connection.to.station.name}</p>
+                            <p><b>Status: </b>{connection.from.delay === 0 ? 'Keine Verspätung' : 'Verspätung'}</p>
+                        </div>
                     </div>
                 </div>
             </div>
