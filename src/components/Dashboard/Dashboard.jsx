6aa3dd253db6d2d7e62a99629cae9db512004cdd
Yanick Minder <minder@puzzle.ch>
2023-05-08 13:38:36 +0200

    make favorites scrollable

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import {retrieveFavorites} from "./dashboard-service";
 import {getPersonalInformation} from "../../helpers/storage";
 import {Navigate} from "react-router-dom";
 import {ConnectionSearch} from "./ConnectionSearch/ConnectionSearch";
+import "./Dashboard.css"
 
 export function Dashboard({isLoggedIn}) {
     const [favorites, setFavorites] = useState([]);
@@ -25,12 +26,14 @@ export function Dashboard({isLoggedIn}) {
                 <div className={"col-6 border border-primary"}>
                     <h1 className={"display-3 m-5"}>Hallo {getPersonalInformation().firstName}</h1>
                     <h3 className={"display-6 mx-5"}>Gespeicherte Verbindungen</h3>
-                    {
-                        favorites.map((favorite => <ListRow
-                            from={favorite.from}
-                            to={favorite.to}
-                            key={favorite.id}/>))
-                    }
+                    <div className="overflow-auto fav-container">
+                        {
+                            favorites.map((favorite => <ListRow
+                                from={favorite.from}
+                                to={favorite.to}
+                                key={favorite.id}/>))
+                        }
+                    </div>
                 </div>
                 <div className="col-6 border border-danger">
                     <ConnectionSearch></ConnectionSearch>
