4dd68cb1d445f9779e69001828887398eacba8d1
megli2 <egli@puzzle.ch>
2023-05-08 11:10:52 +0200

    add logic and frontend to get data of tomorrow

--- src/components/Dashboard/ListRow/ListRow.jsx
+++ src/components/Dashboard/ListRow/ListRow.jsx
@@ -1,10 +1,10 @@
 import {useEffect, useState} from "react";
-import {retrieveConnections} from "./list-row-service";
+import {retrieveConnections, retrieveConnectionsOfTomorrow} from "./list-row-service";
 import {Connection} from "./Connection/Connection";
-import {MINUTE_IN_MILLISECONDS} from "../../../helpers/constants-library";
 
 export function ListRow({from, to, searchParams, setSearchParams}) {
     const [availableConnections, setAvailableConnections] = useState([]);
+    const [showDataOfTomorrow, setShowDataOfTomorrow] = useState(false);
 
     function checkIfShow() {
         return searchParams.get("connection") !== null && (searchParams.get("connection").includes(from)
@@ -22,26 +22,41 @@ export function ListRow({from, to, searchParams, setSearchParams}) {
     function loadConnections() {
         retrieveConnections(from, to)
             .then((response) => {
+                if (response.data.connections.length === 0) {
+                    retrieveConnectionsOfTomorrow(from, to)
+                        .then((responseTomorrow) => {
+                            setShowDataOfTomorrow(true);
+                            setAvailableConnections(responseTomorrow.data.connections);
+                        });
+                }
                 setAvailableConnections(response.data.connections);
             });
     }
 
     useEffect(() => {
         loadConnections();
-        const interval = setInterval(async () => {
-            loadConnections();
-        }, MINUTE_IN_MILLISECONDS);
+        // const interval = setInterval(async () => {
+        //     loadConnections();
+        // }, MINUTE_IN_MILLISECONDS);
 
-        return () => clearInterval(interval);
+        // return () => clearInterval(interval);
     }, []);
 
     return (
         <>
             <div className="accordion-item w-75">
                 <h2 className="accordion-header" id="headingOne">
-                    <button onClick={insertQueryParams} className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    <button onClick={insertQueryParams} className="accordion-button collapsed" type="button"
+                            data-bs-toggle="collapse"
                             data-bs-target={"#" + from + to}>
-                        <h1>{from} -> {to}</h1>
+                        <div className="d-flex flex-row w-100">
+                                <h1 className="w-75">{from} -> {to}</h1>
+                                <h1 className="w-25">
+                                    <i className="bi bi-train-front-fill fs-4">
+                                        {showDataOfTomorrow ? "Morgen" : "Heute"}
+                                    </i>
+                                </h1>
+                        </div>
                     </button>
                 </h2>
                 <div id={from + to}
